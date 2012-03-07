722c4df3edd6379f5af84648093b47d6cacb6cfb
Tatsh <ddrtist@gmail.com>
2012-03-06 22:53:47 -0600

    Update exports

--- export/99-window-export.js
+++ export/99-window-export.js
@@ -4,7 +4,7 @@
  *   Static classes should be plain objects. See fGrammar below.
  *
  * To compile from the root directory:
- * JSARG=""; for i in $(find -iname '*.js' | grep -v fMoney | sort); do JSARG="$JSARG --js $i"; done && closure-compiler $JSARG --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function(){%output%}())" --warning_level VERBOSE
+ * JSARG=""; for i in $(find -iname '*.js' | sort); do JSARG="$JSARG --js $i"; done && closure-compiler $JSARG --js export/99-window-export.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function(){%output%}())" --warning_level VERBOSE
  */
 
 // fCryptography
@@ -12,6 +12,17 @@ window['fCryptography'] = {
   'random': fCryptography.random,
   'randomString': fCryptography.randomString
 };
+
+// fDatabase
+window['fDatabase'] = fDatabase;
+window['fDatabase'].prototype['setDatabase'] = fDatabase.prototype.setDatabase;
+window['fDatabase'].prototype['setConnection'] = fDatabase.prototype.setConnection;
+window['fDatabase'].prototype['isConnected'] = fDatabase.prototype.isConnected;
+window['fDatabase'].prototype['getVersion'] = fDatabase.prototype.getVersion;
+window['fDatabase'].prototype['getConnection'] = fDatabase.prototype.getConnection;
+window['fDatabase'].prototype['getErrorHandler'] = fDatabase.prototype.getErrorHandler;
+window['fDatabase'].prototype['getName'] = fDatabase.prototype.getName;
+window['fDatabase'].prototype['connect'] = fDatabase.prototype.connect;
 
 // fGrammar
 window['fGrammar'] = {
@@ -111,5 +122,22 @@ window['fNumber'].prototype['toFloat'] = fNumber.prototype.toFloat; // special
 
 // fTimestamp
 window['fTimestamp'] = fTimestamp;
-window['fTimestamp'].prototype = fTimestamp.prototype;
 window['fTimestamp'].prototype['getFuzzyDifference'] = fTimestamp.prototype.getFuzzyDifference;
+
+// fMoney
+window['fMoney'] = fMoney;
+window['fMoney']['CURRENCY_INFO_ELEMENT_NAME'] = fMoney.CURRENCY_INFO_ELEMENT_NAME;
+window['fMoney']['CURRENCY_INFO_ELEMENT_PRECISION'] = fMoney.CURRENCY_INFO_ELEMENT_PRECISION;
+window['fMoney']['CURRENCY_INFO_ELEMENT_SYMBOL'] = fMoney.CURRENCY_INFO_ELEMENT_SYMBOL;
+window['fMoney']['CURRENCY_INFO_ELEMENT_VALUE'] = fMoney.CURRENCY_INFO_ELEMENT_VALUE;
+window['fMoney']['defineCurrency'] = fMoney.defineCurrency;
+window['fMoney']['getCurrencies'] = fMoney.getCurrencies;
+window['fMoney']['getCurrencyInfo'] = fMoney.getCurrencyInfo;
+window['fMoney']['reset'] = fMoney.reset;
+window['fMoney']['setDefaultCurrency'] = fMoney.setDefaultCurrency;
+window['fMoney']['getDefaultCurrency'] = fMoney.getDefaultCurrency;
+window['fMoney'].prototype['format'] = fMoney.prototype.format;
+window['fMoney'].prototype['getAmount'] = fMoney.prototype.getAmount;
+window['fMoney'].prototype['getCurrency'] = fMoney.prototype.getCurrency;
+window['fMoney'].prototype['makeMoney'] = fMoney.prototype.makeMoney;
+window['fMoney'].prototype['toString'] = fMoney.prototype.toString;
