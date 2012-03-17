52f8642eb40fa9bbe8184eca658c32f6e43ee4f2
Tatsh <ddrtist@gmail.com>
2012-03-17 17:04:29 -0500

    Fix documentation; do not return anything for static methods

--- fCore.js
+++ fCore.js
@@ -21,7 +21,7 @@ fCore._debugEnabled = false;
   * <li>'windows'</li>
   * </ul>
   *
-  * Try others but the above are guaranteed to work to a degree.
+  * Others may work.
   * @param [args] Variable number of arguments.
   * @returns {boolean} If the current OS is included in the list of OSes passed
   *   as parameters.
@@ -39,17 +39,17 @@ fCore.checkOS = function (args) {
   return false;
 };
 /**
-  * <p>Logs a message. Must call enableDebugging() first with true or this
-  *   method does nothing.</p><p>To see messages in IE versions prior to 8, you
-  *   must have debugging enabled ('Disable Debugging' not checked) and have
-  *   Visual Studio attached to the Script process (<em>Script,x86</em>).</p>
+  * Logs a message.<p>Must call <code>fCore.enableDebugging()</code> first
+  *   with <code>true</code> or this method does nothing.</p><p>To see messages
+  *   in IE versions prior to 8, you must have debugging enabled ('Disable
+  *   Debugging' not checked) and have Visual Studio attached to the Script
+  *   process (<em>Script,x86</em>).</p>
   * @param {string} fmt The format string is composed of zero or more
-  *   directives: ordinary characters (excluding %) that are copied directly to
-  *   the result, and conversion specifications, each of which results in
-  *   fetching its own parameter.
+  *   directives: ordinary characters (excluding <code>%</code>) that are
+  *   copied directly to the result, and conversion specifications, each of
+  *   which results in fetching its own parameter.
   * @param {string|number} [args] Variable number of arguments specified
   *   according to the format string.
-  * @returns {Object} The object to allow method chaining.
   */
 fCore.debug = function (fmt, args) {
   if (fCore._debugEnabled) {
@@ -65,18 +65,14 @@ fCore.debug = function (fmt, args) {
       window.console.log(message);
     }
   }
-
-  return fCore;
 };
 /**
   * Enables or disables debugging globally.
   * @param {boolean} bool Boolean value, true or false.
-  * @returns {Object} The fCore object to allow method chaining.
   * @see fCore#debug
   */
 fCore.enableDebugging = function (bool) {
   fCore._debugEnabled = bool ? true : false;
-  return fCore;
 };
 /**
   * If debugging is enabled.
