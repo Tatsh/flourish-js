a5d9bce54c65797ead98e8930d75517d310bd15d
Tatsh <ddrtist@gmail.com>
2012-03-23 13:23:21 -0500

    Only call sprintf() if the arguments are all compatible

--- fCore.js
+++ fCore.js
@@ -48,25 +48,50 @@ fCore.checkOS = function (args) {
   *   in IE versions prior to 8, you must have debugging enabled ('Disable
   *   Debugging' not checked) and have Visual Studio attached to the Script
   *   process (<em>Script,x86</em>).</p>
-  * @param {string} fmt The format string is composed of zero or more
-  *   directives: ordinary characters (excluding <code>%</code>) that are
-  *   copied directly to the result, and conversion specifications, each of
-  *   which results in fetching its own parameter.
+  * <p>If the arguments passed are valid for sprintf() it will be used.
+  *   Otherwise, the <code>arguments</code> will be logged.</p>
+  * @param {*} fmt The format string is composed of zero or more directives:
+  *   ordinary characters (excluding <code>%</code>) that are copied directly
+  *   to the result, and conversion specifications, each of which results in
+  *   fetching its own parameter.
   * @param {string|number} [args] Variable number of arguments specified
   *   according to the format string.
   */
 fCore.debug = function (fmt, args) {
   if (fCore._debugEnabled) {
-    var message = sprintf.apply(fCore, arguments);
+    var validForSprintf = typeof arguments[0] === 'string';
 
+    // Only strings, numbers, and arrays are allowed for sprintf past the first argument
+    if (validForSprintf) {
+      for (var i = 1; i < arguments.length; i++) {
+        if (objectIsObject(arguments[i])) {
+          validForSprintf = false;
+          break;
+        }
+
+        if (typeof arguments[i] !== 'number' && typeof arguments[i] !== 'string' && !arrayIsArray(arguments[i])) {
+          validForSprintf = false;
+          break;
+        }
+      }
+    }
+
+    // If all of the arguments are compatible with sprintf, call it
+    // Safari/Chrome: Can't save a reference to window.console.log and can't use apply with console.log
+    // IE8 also cannot use apply with console.log
+    if (window.console && window.console.log) {
+      window.console.log(validForSprintf ? sprintf.apply(fCore, arguments) : arguments);
+    }
     if (window['Debug'] && window['Debug'].writeln) {
-      window['Debug'].writeln(message);
+      if (validForSprintf) {
+        window['Debug'].writeln(sprintf.apply(fCore, arguments));
+      }
+      else {
+        window['Debug'].writeln.apply(window, arguments);
+      }
     }
     else if (window['opera'] && window['opera'].postError) {
-      window['opera'].postError(message);
-    }
-    else if (window.console) {
-      window.console.log(message);
+      window['opera'].postError(validForSprintf ? sprintf.apply(fCore, arguments) : arguments);
     }
   }
 };
